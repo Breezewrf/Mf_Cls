5f7c7af899738f1a16b335a7b34a872f0691432d
breezewrf <578450426@qq.com>
2022-12-30 23:14:39 +0800

    modification for MSFusion

--- evaluate.py
+++ evaluate.py
@@ -14,10 +14,15 @@ def evaluate(net, dataloader, device, amp):
     # iterate over the validation set
     with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
         for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', leave=False):
-            image, mask_true = batch['image'], batch['mask']
+            if net.name == 'msf':
+                t2w_img, adc_img, true_masks = batch['t2w_image'], batch['adc_image'], batch['mask']
+                image = torch.stack((t2w_img, adc_img))
+            else:
+                image, true_masks = batch['image'], batch['mask']
 
             # move images and labels to correct device and type
-            image = image.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            # image = image.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            image = image.to(device=device, dtype=torch.float32)
             mask_true = mask_true.to(device=device, dtype=torch.long)
 
             # predict the mask
