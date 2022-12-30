f6915adfc5bf56c753319d56a9e8e8e9a63ea033
breezewrf <578450426@qq.com>
2022-12-30 22:54:59 +0800

    define the MSFusion dataset

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -35,6 +35,91 @@ def unique_mask_values(idx, mask_dir, mask_suffix):
         raise ValueError(f'Loaded masks should have 2 or 3 dimensions, found {mask.ndim}')
 
 
+class MSFDataset(Dataset):
+    def __init__(self, T2W_images_dir: str, ADC_images_dir: str, mask_dir: str, scale: float = 1.0):
+        self.t2w_dir = Path(T2W_images_dir)
+        self.adc_dir = Path(ADC_images_dir)
+        self.mask_dir = Path(mask_dir)
+        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
+        self.scale = scale
+        self.ids = [splitext(file)[0] for file in listdir(T2W_images_dir) if
+                    isfile(join(T2W_images_dir, file)) and not file.startswith('.')]
+        if not self.ids:
+            raise RuntimeError(f'No input file found in {T2W_images_dir}, make sure you put your images there')
+        logging.info(f'Creating dataset with {len(self.ids)} examples')
+        logging.info('Scanning mask files to determine unique values')
+        with Pool() as p:
+            unique = list(tqdm(
+                p.imap(partial(unique_mask_values, mask_dir=self.mask_dir, mask_suffix=''), self.ids),
+                total=len(self.ids)
+            ))
+
+        self.mask_values = list(sorted(np.unique(np.concatenate(unique), axis=0).tolist()))
+        logging.info(f'Unique mask values: {self.mask_values}')
+
+    @staticmethod
+    def preprocess(mask_values, pil_img, scale, is_mask=True, target_size=256):
+        w, h = pil_img.size
+        # newW, newH = int(scale * w), int(scale * h)
+        newW = target_size
+        newH = target_size
+        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
+        pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
+        img = np.asarray(pil_img)
+
+        if is_mask:
+            mask = np.zeros((newH, newW), dtype=np.int64)
+            for i, v in enumerate(mask_values):
+                if img.ndim == 2:
+                    mask[img == v] = i
+                else:
+                    mask[(img == v).all(-1)] = i
+
+            return mask
+
+        else:
+            if img.ndim == 2:
+                img = img[np.newaxis, ...]
+            else:
+                img = img.transpose((2, 0, 1))
+
+            if (img > 1).any():
+                img = img / 255.0
+
+            return img
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+        name = self.ids[idx]
+        mask_file = list(self.mask_dir.glob(name + '.*'))
+        t2w_file = list(self.t2w_dir.glob(name + '.*'))
+        adc_file = list(self.adc_dir.glob(name + '.*'))
+
+        assert len(t2w_file) == 1, f'Either no image or multiple images found for the ID {name} in t2w: {t2w_file}'
+        assert len(adc_file) == 1, f'Either no image or multiple images found for the ID {name} in adc: {adc_file}'
+        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
+        mask = load_image(mask_file[0])
+        t2w_img = load_image(t2w_file[0])
+        adc_img = load_image(adc_file[0])
+
+        assert adc_img.size == mask.size, \
+            f'adc Image and mask {name} should be the same size, but are {adc_img.size} and {mask.size}'
+        assert t2w_img.size == mask.size, \
+            f't2w Image and mask {name} should be the same size, but are {t2w_img.size} and {mask.size}'
+
+        t2w_img = self.preprocess(self.mask_values, t2w_img, self.scale, is_mask=False)
+        adc_img = self.preprocess(self.mask_values, adc_img, self.scale, is_mask=False)
+        mask = self.preprocess(self.mask_values, mask, self.scale, is_mask=True)
+
+        return {
+            't2w_image': torch.as_tensor(t2w_img.copy()).float().contiguous(),
+            'adc_image': torch.as_tensor(adc_img.copy()).float().contiguous(),
+            'mask': torch.as_tensor(mask.copy()).long().contiguous()
+        }
+
+
 class BasicDataset(Dataset):
     def __init__(self, images_dir: str, mask_dir: str, scale: float = 1.0, mask_suffix: str = ''):
         self.images_dir = Path(images_dir)
@@ -43,7 +128,8 @@ class BasicDataset(Dataset):
         self.scale = scale
         self.mask_suffix = mask_suffix
 
-        self.ids = [splitext(file)[0] for file in listdir(images_dir) if isfile(join(images_dir, file)) and not file.startswith('.')]
+        self.ids = [splitext(file)[0] for file in listdir(images_dir) if
+                    isfile(join(images_dir, file)) and not file.startswith('.')]
         if not self.ids:
             raise RuntimeError(f'No input file found in {images_dir}, make sure you put your images there')
 
