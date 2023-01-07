9bfe8d55c4e72d5835f97ff04e803df9172d7048
breezewrf <578450426@qq.com>
2023-01-07 12:13:03 +0800

    path_error

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #SBATCH -w node3
 
 source /home/rfwei2/wrf/Mf_Cls/env/bin/activate
-python3 train.py --amp --epochs 400 -l 5e-7 -b 4 --model msf --seed 57749867
-python3 train.py --amp --epochs 400 -l 1e-8 -b 4 --model msf --seed 57749867
-python3 train.py --amp --epochs 400 -l 1e-7 -b 4 --model msf --seed 57749867
-
+python3 train.py --amp --epochs 512 -l 1e-8 -b 1 --model msf --seed 57749867
+python3 train.py --amp --epochs 512 -l 1e-8 -b 1 --model msf --seed 57749867
+python3 train.py --amp --epochs 512 -l 1e-8 -b 1 --model msf --seed 57749867
+python3 train.py --amp --epochs 512 -l 1e-8 -b 1 --model msf --seed 57749867
