4621792a0150917db12d571b15dcc062a86bb0db
breezewrf <578450426@qq.com>
2023-03-24 17:28:46 +0800

    code for Class Activation Map and GAIN training

---
+++ train_cam.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+# @Time    : 24/3/2023 3:47 PM
+# @Author  : Breeze
+# @Email   : breezewrf@gmail.com
+import argparse
+import logging
+import torch
+from msf_cls.backbone.resnet import resnet34, resnet18, resnet50, resnet101
+from msf_cls.backbone.convnext import ConvNeXt
+from msf_cls.backbone.vgg import Vgg_16
+from util.data_loading import Cls_Dataset, Cls_ProstateX_Dataset
+import os
+import random
+from torch.utils.data import DataLoader, random_split, WeightedRandomSampler
+import numpy as np
+from torch import optim
+from tqdm import tqdm
+from evaluate import evaluate_cls
+from pathlib import Path
+import wandb
+from sklearn.model_selection import KFold
+from msf_cls.backbone.gain import GAIN
+
+os.environ["WANDB_MODE"] = "offline"
+dir_checkpoint = Path('./checkpoints/classification/')
+from loss import lw_loss
+from msf_cls.backbone.pretrained import Resnet_18, VGG16
+from loss import FocalLoss
+
+kf = KFold(n_splits=5, shuffle=True, random_state=57749867)
+focalLoss = FocalLoss(alpha=1, gamma=2)
+
+
+def train_model(
+        model_name,
+        device,
+        epochs: int = 2,
+        batch_size: int = 1,
+        learning_rate: float = 3e-4,
+        val_percent: float = 0.1,
+        save_checkpoint: bool = True,
+        save_interval: int = 50,
+        img_scale: float = 0.5,
+        amp: bool = False,
+        weight_decay: float = 1e-8,
+        momentum: float = 0.999,
+        gradient_clipping: float = 1.0,
+        branch: int = 1,
+        seed=12321,
+        aug=1,
+        opt='adamw',
+        desc='',
+        num_classes=2,
+        log=True
+):
+    best_model_path = 'best.pth'
+    if log:
+        config = {'epoch': epochs, 'batch_size': batch_size, 'lr': learning_rate, 'seed': seed, 'opt': opt}
+        run = wandb.init(project='classification', config=config)
+
+    # 1. create dataset
+    dataset = Cls_ProstateX_Dataset(num_classes=num_classes)
+    test_percent = 0.2
+    n_test = int(len(dataset) * test_percent)
+    n_train_val = len(dataset) - n_test
+    train_val_set, test_set = random_split(dataset, [n_train_val, n_test],
+                                           generator=torch.Generator().manual_seed(seed))
+    # n_val = int(len(dataset) * val_percent)
+    # n_train = len(dataset) - n_val
+
+    # (1) Set `os env`
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    # (2) Set `python` built-in pseudo-random generator at a fixed value
+    random.seed(seed)
+    # (3) Set `numpy` pseudo-random generator at a fixed value
+    np.random.seed(seed)
+    # (4) Set `torch`
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    global best_acc
+    best_acc = 0
+    best_epoch = 0
+    # 3. Create data loaders
+    for fold, (train_idx, val_idx) in enumerate(kf.split(train_val_set)):
+        model_list = {'resnet18': Resnet_18(num_classes=num_classes), 'resnet34': resnet34(), 'resnet50': resnet50(),
+                      'resnet101': resnet101(),
+                      'vgg16': VGG16(), 'convnext': ConvNeXt()}
+        logging.info(f'Using device {device}')
+        assert model_name in model_list
+        model = GAIN(num_classes=num_classes)
+        model = model.to(device)
+
+        train_set = torch.utils.data.Subset(dataset, train_idx)
+        val_set = torch.utils.data.Subset(dataset, val_idx)
+        n_train = len(train_set)
+        # re-weight
+        class_weight = np.zeros(num_classes)
+        train_labels = []
+        for im, label in train_set:
+            l, t = np.unique(label, return_counts=True)
+            class_weight[l] += t
+            train_labels.append(label)
+        exp_weight = [(1 - c / sum(class_weight)) ** 2 for c in class_weight]
+        example_weight = [exp_weight[e] for e in train_labels]
+        sampler = WeightedRandomSampler(example_weight, len(train_labels))
+        loader_args = dict(batch_size=batch_size, num_workers=os.cpu_count(), pin_memory=True)
+        train_loader = DataLoader(train_set, sampler=sampler, **loader_args)
+        val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
+        test_lodaer = DataLoader(test_set, shuffle=False, **loader_args)
+        # exp_weight = [1, 0, 0, 0]
+        class_weight = np.zeros(num_classes)
+        for im, label in train_loader:
+            l, t = np.unique(label, return_counts=True)
+            class_weight[l] += t
+        logging.info("class weight after re_weight: {}".format(class_weight))
+
+        # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
+        optimizer = optim.AdamW(model.parameters(),
+                                lr=learning_rate)  # , weight_decay=weight_decay, momentum=momentum, foreach=True)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=60,
+                                                         factor=0.5)
+        grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
+        global_step = 0
+        # 5. Begin training
+        for epoch in range(1, epochs + 1):
+            model.train()
+            epoch_loss = 0
+            with tqdm(total=n_train, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
+                for img, grade in train_loader:
+                    img = img.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+                    grade = grade.to(device=device, dtype=torch.float32)
+                    model.train()
+                    with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
+                        loss = model(img, grade)
+
+                    optimizer.zero_grad(set_to_none=True)
+                    grad_scaler.scale(loss).backward()
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clipping)
+                    grad_scaler.step(optimizer)
+                    grad_scaler.update()
+                    pbar.update(img.shape[0])
+                    global_step += 1
+                    epoch_loss += loss.item()
+                    pbar.set_postfix(**{'loss (batch)': loss.item()})
+                    if log:
+                        wandb.log({'loss': loss.item()})
+                    division_step = (n_train // (5 * batch_size))
+                    if division_step > 0:
+                        if global_step % division_step == 0:
+                            score = evaluate_cls(model.model, val_loader, device, amp, args.model,
+                                                 batch_size=batch_size)
+                            scheduler.step(score)
+                            logging.info('Score: {}'.format(score))
+                            if log:
+                                wandb.log({'score': score})
+            if save_checkpoint and epoch % save_interval == 0:
+                test_acc = evaluate_cls(model, test_lodaer, device, amp, args.model, batch_size=batch_size)
+                print("test_score:{}".format(test_acc))
+                Path(dir_checkpoint).mkdir(parents=True, exist_ok=True)
+                state_dict = model.state_dict()
+                torch.save(state_dict, str(dir_checkpoint / 'checkpoint_epoch{}.pth'.format(epoch)))
+                logging.info(f'Checkpoint {epoch} saved!')
+                wandb.log({"test_acc": test_acc})
+                if test_acc > best_acc:
+                    best_acc = test_acc
+                    best_epoch = epoch
+                    torch.save(state_dict,
+                               str(Path('epoch' + str(epochs)) / '{}'.format(best_model_path)))
+                    if log:
+                        model_wandb = wandb.Artifact('classification-model', type='model')
+                        model_wandb.add_file(str(Path('epoch' + str(epochs)) / '{}_final.pth'.format(desc)))
+                        run.log_artifact(model_wandb)
+
+        logging.info("best model is trained with {} epochs, best acc is {}".format(best_epoch, best_acc))
+
+        logging.info(f'Checkpoint  training finished!')
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Train the Classification')
+    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=100, help='Number of epochs')
+    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=4, help='Batch size')
+    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=3e-8,
+                        help='Learning rate', dest='lr')
+    parser.add_argument('--load', '-f', type=str,
+                        default=False,
+                        help='Load model from a .pth file')
+    parser.add_argument('--scale', '-s', type=float, default=1, help='Downscaling factor of the images')
+    parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
+                        help='Percent of the data that is used as validation (0-100)')
+    parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
+    parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
+    parser.add_argument('--classes', '-c', type=int, default=4, help='Number of classes')
+    parser.add_argument('--model', type=str, default='resnet18',
+                        help='choose model from: resnet18, resnet34, resnet50,resnet101, vgg16, convnext, mfcls')
+    parser.add_argument('--branch', type=int, default=2, help='denotes the number of streams')
+    parser.add_argument('--seed', type=int, default=12321)
+    parser.add_argument('--aug', type=int, default=1, help='set aug equal to 1 to implement augmentation')
+    parser.add_argument('--opt', type=str, default='adamw')
+    parser.add_argument('--log', type=bool, default=True)
+    parser.add_argument('--desc', type=str)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_args()
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    logging.info("classification model: {}".format(args.model))
+    logging.info("classes number: {}".format(args.classes))
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    train_model(
+        model_name=args.model,
+        epochs=args.epochs,
+        batch_size=args.batch_size,
+        learning_rate=args.lr,
+        device=device,
+        img_scale=args.scale,
+        val_percent=args.val / 100,
+        amp=args.amp,
+        branch=args.branch,
+        seed=args.seed,
+        aug=args.aug,
+        opt=args.opt,
+        desc=args.desc,
+        num_classes=args.classes,
+        log=True
+    )
---
+++ util/visualize.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+# @Time    : 19/3/2023 10:22 PM
+# @Author  : Breeze
+# @Email   : breezewrf@gmail.com
+import glob
+import PIL.ImageDraw
+import PIL.Image
+import cv2
+import numpy as np
+import torch
+from torchvision import models, transforms
+from msf_cls.backbone.pretrained import Resnet_18
+from util.utils import imshow
+from util.data_loading import Cls_ProstateX_Dataset
+from pytorch_grad_cam import GradCAM, HiResCAM, GradCAMElementWise, GradCAMPlusPlus, XGradCAM, AblationCAM, ScoreCAM, \
+    EigenCAM, EigenGradCAM, LayerCAM, FullGrad, DeepFeatureFactorization
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from matplotlib import pyplot as plt
+from torch.nn import functional as F
+from matplotlib import colors
+
+cam_list = ['GradCAM', 'HiResCAM', 'GradCAMElementWise', 'GradCAMPlusPlus', 'XGradCAM', 'AblationCAM', 'ScoreCAM',
+            'EigenCAM', 'EigenGradCAM', 'LayerCAM', 'FullGrad', 'mask']
+
+
+def plot_imgs(images):
+    # Create a 3x4 grid of subplots
+    fig, axs = plt.subplots(3, 4, figsize=(10, 8))
+    axs = axs.ravel()
+
+    # Loop through each image and plot it on a subplot
+    for i in range(12):
+        axs[i].imshow(np.squeeze(images[i]), cmap='viridis')
+        axs[i].set_title(f"{cam_list[i]}")
+    plt.show()
+
+
+def Generate_CAMs(model, im):
+    cam = GradCAM(model=model.model, target_layers=[model.model.layer4[-1]])
+    targets = [ClassifierOutputTarget(3)]
+    grayscale_cam = cam(input_tensor=im.unsqueeze(0), targets=targets)
+    # imshow(grayscale_cam.transpose(1, 2, 0))
+    color_map = (cv2.applyColorMap((grayscale_cam * 255).astype(np.uint8).transpose(1, 2, 0), cv2.COLORMAP_HSV))
+    # imshow(color_map)
+    map = ((im.numpy() * 255).transpose(1, 2, 0) * 0.5 + color_map * 0.5)
+    # imshow(map)
+    cams = [GradCAM, HiResCAM, GradCAMElementWise, GradCAMPlusPlus, XGradCAM, AblationCAM, ScoreCAM,
+            EigenCAM, EigenGradCAM, LayerCAM, FullGrad]
+    im_np = im.numpy().transpose(1, 2, 0)
+    h, w, _ = im_np.shape
+    data = []
+    for idx, method in enumerate(cams):
+        print(cam)
+        cam = method(model=model.model, target_layers=[model.model.layer4[-1]])
+        grayscale_cam = cam(input_tensor=im.unsqueeze(0), targets=targets)
+        # imshow(grayscale_cam.transpose(1, 2, 0), desc=str(cam))
+        data.append(grayscale_cam.transpose(1, 2, 0))
+    # cam_color = cv2.applyColorMap(cv2.resize(grayscale_cam.astype(np.uint8).transpose(1, 2, 0), (w, h)), cv2.COLORMAP_HSV)
+    # imshow(cam_color)
+    # cam_img = im_np * 255 * 0.5 + cam_color * 0.5
+    # imshow(cam_img)
+    # cam = ScoreCAM(model=model.model, target_layers=[model.model.layer4[-1]])
+    # grayscale_cam = cam(input_tensor=im.unsqueeze(0), targets=targets)
+    # imshow(grayscale_cam.transpose(1, 2, 0), desc=str(cam))
+    return data
+
+
+# def get_CAM_weights(model, input: torch.Tensor, last_conv_layer_name, fc_layer_name):
+#     last_conv_layer = model._modules.get(last_conv_layer_name)
+#     fc_layer = model._modules.get(fc_layer_name)[-1]
+#     # Get the weights from the fully connected layer
+#     fc_weights = np.squeeze(fc_layer.weight.data.cpu().numpy())
+#     # Get the activations from the last convolutional layer
+#     activations = {}
+#
+#     def hook_fn(name):
+#         def hook(module, input, output):
+#             activations[name] = output.detach()
+#
+#         return hook
+#
+#     last_conv_layer.register_forward_hook(hook_fn(last_conv_layer_name))
+#     # Make a forward pass to get the model prediction
+#     output = model(input)
+#     pred_class = np.argmax(output.detach().numpy())
+#     # Get the activations and compute the CAM weights
+#     activations = activations[last_conv_layer_name].squeeze().cpu().numpy()
+#     CAM_weights = fc_weights[pred_class].dot(activations.reshape(activations.shape[0], -1)).reshape(
+#         activations.shape[1], activations.shape[2])
+#     return CAM_weights
+#
+#
+# def get_attention_map(img, CAM_weights):
+#     # Resize the CAM weights to the size of the image
+#     CAM_weights = cv2.resize(CAM_weights, (img.shape[1], img.shape[0]))
+#     # Apply a ReLU activation to the CAM weights
+#     CAM_weights = np.maximum(CAM_weights, 0)
+#     # Normalize the CAM weights
+#     CAM_weights = CAM_weights / CAM_weights.max()
+#     # Convert the CAM weights to an RGB image
+#     CAM_weights = np.uint8(CAM_weights)
+#     CAM_weights = cv2.applyColorMap(CAM_weights, cv2.COLORMAP_HSV)
+#     # Combine the CAM weights and the original image
+#     attention_map = CAM_weights.astype(np.float64) * 0.5 + img.astype(np.float64) * 0.5
+#     attention_map = attention_map / attention_map.max()
+#     return attention_map
+
+
+def get_img(id=100):
+    dataset = Cls_ProstateX_Dataset(label_dir='/media/breeze/dev/Mf_Cls/data/ProstateX/labeled_GT_colored/',
+                                    test_mode=True)
+    # show image that used for training
+    im, label = dataset[id]
+    imshow(im.transpose(0, 1).transpose(1, 2), desc="used for training")
+    # show lesion that cropped from mask
+    img = dataset.lesions[id].image_np
+    imshow(img, desc="cropped lesion")
+
+    slice_id = dataset.lesions[id].slice_id
+    patient_id = dataset.lesions[id].patient_id
+    path = glob.glob(
+        '/media/breeze/dev/Mf_Cls/data/ProstateX/labeled_GT_colored/ProstateX-{}-{}-*'.format(str(patient_id).zfill(4),
+                                                                                              str(slice_id)))
+    # '/media/breeze/dev/Mf_Cls/data/ProstateX/label_color/ProstateX-{}-{}-*.jpg'.format(patient_id, slice_id))
+    mask = PIL.Image.open(path[0])
+
+    # show the rectangle in mask
+    draw = PIL.ImageDraw.Draw(mask)
+    draw.rectangle((dataset.lesions[id].bbox[0][1], dataset.lesions[id].bbox[0][0], dataset.lesions[id].bbox[0][3],
+                    dataset.lesions[id].bbox[0][2]))
+    imshow(PIL.Image.fromarray(np.array(mask) * 255), desc="mask")
+
+    # show the cropped mask by rectangle
+    mask_ = PIL.Image.fromarray(np.uint8((mask)) * 255).crop((dataset.lesions[id].bbox[0][1],
+                                                              dataset.lesions[id].bbox[0][0],
+                                                              dataset.lesions[id].bbox[0][3],
+                                                              dataset.lesions[id].bbox[0][2]))
+    imshow(mask_, desc="rectangle mask")
+
+    return im, mask_
+
+
+def CAM(model, im):
+    pred = model(im.unsqueeze(0))
+    c = np.argmax(pred.data.numpy())
+    fc = model.model.fc[-1].state_dict()['weight'][c]  # shape[512]
+    if len(im.shape) == 3:
+        im = im.unsqueeze(0)
+    im_ = model.model.conv1(im)
+    im_ = model.model.bn1(im_)
+    im_ = model.model.relu(im_)
+    im_ = model.model.maxpool(im_)
+    im_ = model.model.layer1(im_)
+    im_ = model.model.layer2(im_)
+    im_ = model.model.layer3(im_)
+    im_ = model.model.layer4(im_)
+    assert im_.shape == torch.Size([1, 512, 7, 7])
+    im_flatten = im_.view(-1, 512, 7 * 7)
+    activation_map = torch.zeros((7 * 7, 512))
+    for i in range(0, activation_map.shape[0]):
+        m = fc * im_flatten[0, :, i]
+        activation_map[i] = m
+    up = F.upsample_bilinear(input=activation_map.sum(dim=1).reshape(7, 7).unsqueeze(dim=0).unsqueeze(dim=0),
+                             size=(64, 64))
+    imshow(up[0][0], desc="CAM_1")
+
+
+def CAM_hook(model, im):
+    feature_data = []
+
+    def feature_hook(model, input, output):
+        feature_data.append(output.data.numpy())
+
+    model.model._modules.get('layer4').register_forward_hook(feature_hook)
+    fc_weight = model.model._modules.get('fc')[-1].weight.data.numpy()
+    pred = model(im.unsqueeze(0))
+    pred_c = np.argmax(pred.data.numpy())
+
+    def makeCAM(feature, weights, classes_id):
+        print(feature.shape, weights.shape, classes_id.shape)
+        bz, nc, h, w = feature.shape
+        cam = weights[classes_id].dot(feature.reshape(nc, h * w))
+        cam = cam.reshape(h, w)
+        cam = (cam - cam.min()) / (cam.max() - cam.min())
+        cam_gray = np.uint8(255 * cam)
+        return cv2.resize(cam_gray, (224, 224))
+
+    cam = makeCAM(feature_data[0], fc_weight, pred_c)
+    imshow(cam)
+    im_np = im.numpy().transpose(1, 2, 0)
+    h, w, _ = im_np.shape
+    cam_color = cv2.applyColorMap(cv2.resize(cam, (w, h)), cv2.COLORMAP_HSV)
+    imshow(cam_color)
+    cam_img = im_np * 255 * 0.5 + cam_color * 0.5
+    imshow(cam_img)
+
+
+if __name__ == '__main__':
+    # Load the model
+    model = Resnet_18(num_classes=4)
+    checkpoint_path = '/media/breeze/dev/Mf_Cls/checkpoints/classification/checkpoint_epoch100.pth'
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint)
+    model.eval()
+
+    # Get the CAM weights
+
+    # Load and preprocess the input image
+
+    # img_tensor = preprocess(img).unsqueeze(0)
+    im, mask_ = get_img(id=45)
+    # img_tensor = im.unsqueeze(0)
+    # CAM_weights = get_CAM_weights(model.model, img_tensor, 'layer4', 'fc')
+    #
+    # # Get the attention map
+    # attention_map = get_attention_map(im.transpose(0, 1).transpose(1, 2).numpy(), CAM_weights)
+    # imshow(attention_map)
+    CAM(model, im)
+    CAM_hook(model, im)
+    datas = Generate_CAMs(model, im)
+    datas.append(mask_.resize((224, 224), resample=2))
+    plot_imgs(datas)
